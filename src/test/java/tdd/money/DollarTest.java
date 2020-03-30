d27abc92fc1aa8c1257928ffad3adfd306f79671
themorning <themorning84@gmail.com>
2020-03-31 01:09:30 +0900

    팩토리메서드로 테스트

--- src/test/java/tdd/money/DollarTest.java
+++ src/test/java/tdd/money/DollarTest.java
@@ -12,9 +12,9 @@ public class DollarTest {
 
         Money five = Money.dollar(5);
 
-        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(Money.dollar(10), five.times(2));
 
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
@@ -28,11 +28,11 @@ public class DollarTest {
 
     @Test
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Money.dollar(5)));
 
     }
 }
