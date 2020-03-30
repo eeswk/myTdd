3e4463cbc151af367e8056bf5b13117ed9adc8a5
themorning <themorning84@gmail.com>
2020-03-31 01:02:29 +0900

    Franc와 Doller 클래스 비교

--- src/test/java/tdd/money/DollarTest.java
+++ src/test/java/tdd/money/DollarTest.java
@@ -2,8 +2,7 @@ package tdd.money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DollarTest {
 
@@ -29,6 +28,10 @@ public class DollarTest {
     @Test
     void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
 
     }
 }
--- src/test/java/tdd/money/Franc.java
+++ src/test/java/tdd/money/Franc.java
@@ -1,7 +1,6 @@
 package tdd.money;
 
 public class Franc extends Money{
-    private int amount;
 
     public Franc(int amount) {
         this.amount = amount;
@@ -11,9 +10,4 @@ public class Franc extends Money{
         return new Franc(amount * mulitipier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        Franc dollar = (Franc) o;
-        return amount == dollar.amount;
-    }
 }
