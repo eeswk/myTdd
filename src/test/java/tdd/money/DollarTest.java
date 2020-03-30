f0664c4727aece6257654193281b5a041a1f470a
themorning <themorning84@gmail.com>
2020-03-31 00:54:33 +0900

    Franc 추가 및 테스트

--- src/test/java/tdd/money/DollarTest.java
+++ src/test/java/tdd/money/DollarTest.java
@@ -18,6 +18,15 @@ public class DollarTest {
     }
 
     @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+
+        assertEquals(new Franc(10), five.times(2));
+
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
 
---
+++ src/test/java/tdd/money/Franc.java
@@ -0,0 +1,19 @@
+package tdd.money;
+
+public class Franc {
+    private int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int mulitipier) {
+        return new Franc(amount * mulitipier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Franc dollar = (Franc) o;
+        return amount == dollar.amount;
+    }
+}
