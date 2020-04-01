f8dab3859cb10f1df02409bf8a4e2236840b4d68
themorning <themorning84@gmail.com>
2020-04-02 00:35:16 +0900

    캐스팅과 클래스 검사 코드 제거

--- src/test/java/tdd/money/Bank.java
+++ src/test/java/tdd/money/Bank.java
@@ -2,12 +2,14 @@ package tdd.money;
 
 public class Bank {
     public Money reduce(Expression source, String currency) {
+        return source.reduce(currency);
+
         //return Money.dollar(10);
-        if (source instanceof Money)
-            return (Money) source.reduce(currency);
-        Sum sum = (Sum)source;
+        //if (source instanceof Money)
+        //    return (Money) source.reduce(currency);
+        //Sum sum = (Sum)source;
         //int amount = sum.augend.amount + sum.addend.amount;
-        return sum.reduce(currency);
+        //return sum.reduce(currency);
         //return new Money(amount, currency);
     }
 }
