011f01bb6640136c4f9378c606647410ad189bfd
themorning <themorning84@gmail.com>
2020-04-02 00:24:56 +0900

    더하기 계산 테스트

--- src/test/java/tdd/money/Bank.java
+++ src/test/java/tdd/money/Bank.java
@@ -2,6 +2,9 @@ package tdd.money;
 
 public class Bank {
     public Money reduce(Expression source, String currency) {
-        return Money.dollar(10);
+        //return Money.dollar(10);
+        Sum sum = (Sum)source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, currency);
     }
 }
