2bb8cc748aa29de6c5cf101600e06acb847574fb
themorning <themorning84@gmail.com>
2020-04-02 00:27:37 +0900

    Sum클래스에 위임

--- src/test/java/tdd/money/Bank.java
+++ src/test/java/tdd/money/Bank.java
@@ -4,7 +4,8 @@ public class Bank {
     public Money reduce(Expression source, String currency) {
         //return Money.dollar(10);
         Sum sum = (Sum)source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, currency);
+        //int amount = sum.augend.amount + sum.addend.amount;
+        return sum.reduce(currency);
+        //return new Money(amount, currency);
     }
 }
